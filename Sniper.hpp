db40389e099daf41c540d42195ed38a94ced7c03
meirfuces <meirfuces@gmail.com>
2020-06-01 19:24:56 +0300

    add attack to Sniper

--- Sniper.hpp
+++ Sniper.hpp
@@ -9,6 +9,50 @@ namespace WarGame {
         }
 
         //fuction Here
-         void shoot1(); //need to change virtoul
+        void attack(std::vector<std::vector<Soldier*>> &board, std::pair<int,int> source)override {
+            Soldier *enemy_to_attack = find_soldier_attack(board, source);
+            cout << "The life of enemy is " <<enemy_to_attack->hp<< endl;
+            if (enemy_to_attack != nullptr) {
+                enemy_to_attack->hp -= 50;
+
+                if (enemy_to_attack->hp <= 0) {
+                    //delete enemy_to_attack;
+                    //need to Add destractor
+                    delete enemy_to_attack;
+                    cout << "Sniper Attack and kill" << endl;// dead
+                }
+                cout << "Sniper Attack The life of enemy is " <<enemy_to_attack->hp<< endl; // alive
+            }
+            cout << "No Attack" << endl; // alive
+        }
+
+        Soldier* find_soldier_attack (std::vector<std::vector<Soldier*>> &board, std::pair<int,int> source){
+            Soldier* ans;
+            std::pair<int, int> myLocation = {source.first, source.second};
+            double maxnHealth = 0;
+            int HelthTemp;
+            for (int i = 0; i < board.size(); ++i) {
+                for (int j = 0; j < board.size(); ++j) {
+                    if (board[i][j] != nullptr && board[i][j]->player != this->player) {
+                        HelthTemp = board[i][j]->hp;
+                        if (maxnHealth < HelthTemp) {
+                            ans = board[i][j];
+                            maxnHealth = HelthTemp;
+                        }
+                    }
+                }
+            }
+            if (ans!=nullptr)
+                return ans;
+            return nullptr;
+        }
+
+        void fillLife() override{
+            this->hp = 100;
+        }
+        ~Sniper()
+        {
+            delete this;
+        }
     };
 }
